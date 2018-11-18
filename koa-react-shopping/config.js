189a139f7ecf3a7593be4576909de592ce046c42
shiyong <15797675808@163.com>
2018-11-18 20:51:37 +0800

    :arrow_up: branch dev backend

---
+++ koa-react-shopping/config.js
@@ -0,0 +1,14 @@
+const config = {
+
+  port: 3001,
+
+  database: {
+    DATABASE: 'koa2_db',
+    USERNAME: 'root',
+    PASSWORD: 'shiyong001',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
